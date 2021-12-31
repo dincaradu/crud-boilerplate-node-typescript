4b2385bbc364ba52c17f5f440fd65c8f4ca51954
Radu Dinca <dinca.radu@gmail.com>
2021-12-31 04:20:19 +0200

    Add .dotenv support

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
+import { ENVIRONMENT, PORT } from "./config/env.config";
 
 const app = express();
-const port = 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
